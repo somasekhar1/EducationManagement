5a4a2cff8ffca569c02e9b36772ea57f0a2154ce
Somasekhar Reddy <44517803+somasekhar1@users.noreply.github.com>
2020-12-24 22:23:24 +0530

    change

--- EducationManagement/settings.py
+++ EducationManagement/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'h2gt#1*^)^@@j!^^#ffcee4co!+7)^r)psn!=lw$f879aqn@rp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','sampledomain.com']
+ALLOWED_HOSTS = ['127.0.0.1','education-management.herokuapp.com']
 
 
 # Application definition
