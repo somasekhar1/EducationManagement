d53f13f8ac86b53ae2046720d1cd77e3c37a0fdc
Somasekhar Reddy <44517803+somasekhar1@users.noreply.github.com>
2020-12-24 23:04:07 +0530

    Update settings.py

--- EducationManagement/settings.py
+++ EducationManagement/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'h2gt#1*^)^@@j!^^#ffcee4co!+7)^r)psn!=lw$f879aqn@rp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG =True
 
-ALLOWED_HOSTS = ['127.0.0.1','education-management.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
